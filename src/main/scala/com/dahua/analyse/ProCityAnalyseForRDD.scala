56f8b57a4033444f0fcb24ef64e8fc741458211b
lllhq <1802332496@qq.com>
2022-03-28 15:46:22 +0800

    update

--- src/main/scala/com/dahua/analyse/ProCityAnalyseForRDD.scala
+++ src/main/scala/com/dahua/analyse/ProCityAnalyseForRDD.scala
@@ -1,10 +1,10 @@
 package com.dahua.analyse
 
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.{FileSystem, Path}
-import org.apache.spark.{HashPartitioner, SparkConf}
+import org.apache.spark.{Partitioner, SparkConf}
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.SparkSession
+
+import scala.collection.mutable
 
 object ProCityAnalyseForRDD {
 
@@ -14,7 +14,7 @@ object ProCityAnalyseForRDD {
       println(
         """
           |缺少参数
-          |inputpath  outputpath
+          |inputpath outputpath
           |""".stripMargin)
       sys.exit()
     }
@@ -22,36 +22,55 @@ object ProCityAnalyseForRDD {
     // 创建sparksession对象
     var conf = new SparkConf().set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
 
-    val spark = SparkSession.builder().config(conf).appName("Log2Parquet").master("local[1]").getOrCreate()
+    val spark = SparkSession.builder().config(conf).appName("Log2Parquet").getOrCreate()
 
     var sc = spark.sparkContext
 
-    //接收参数
     var Array(inputPath, outputPath) = args
 
     val line: RDD[String] = sc.textFile(inputPath)
-    val field: RDD[Array[String]] = line.map(_.split(",", -1))
-    val proCityRDD: RDD[((String, String), Int)] = field.filter(_.length >= 85).map(arr => {
+    val filed: RDD[Array[String]] = line.map(_.split(",", -1))
+    val procityRDD: RDD[((String, String), Int)] = filed.filter(_.length >= 85).map(arr => {
       var pro = arr(24)
       var city = arr(25)
       ((pro, city), 1)
     })
+    val reduceRDD: RDD[((String, String), Int)] = procityRDD.reduceByKey(_ + _)
 
-    val reduceRDD: RDD[((String, String), Int)] = proCityRDD.reduceByKey(_ + _)
-
-    val rdd2: RDD[(String, ((String, String), Int))] = reduceRDD.map(arr => {
-      (arr._1._1, (arr._1, arr._2))
+    val rdd2: RDD[(String, (String, Int))] = reduceRDD.map(x => {
+      (x._1._1, (x._1._2, x._2))
     })
 
-    val num: Long = rdd2.map(x => {
-      (x._1, 1)
-    }).reduceByKey(_ + _).count()
 
-    rdd2.partitionBy(new HashPartitioner(num.toInt)).saveAsTextFile(outputPath)
+    val num: Int = rdd2.map(x => {
+      (x._1)
+    }).distinct().count().toInt
+
+    reduceRDD.sortBy(-_._2).coalesce(1).partitionBy(new MyPartition(num)).saveAsTextFile(outputPath)
 
     spark.stop()
     sc.stop()
+  }
+}
 
+class MyPartition(val count: Int) extends Partitioner {
+  override def numPartitions: Int = count
+
+  private var num = -1
+
+  private val map: mutable.Map[String, Int] = mutable.Map[String, Int]()
+
+  override def getPartition(key: Any): Int = {
+    val value: String = key.toString
+    val str: String = value.substring(1, value.indexOf(","))
+
+    if (map.contains(str)) {
+      map.getOrElse(str, num)
+    } else {
+      num += 1
+      map.put(str, num)
+      num
+    }
 
   }
 

cf17e82cbca834f84065c5673bcd98ed4ab7d0fe
lllhq <1802332496@qq.com>
2022-04-24 14:35:26 +0800

    update

--- src/main/scala/com/dahua/tag/TagRptToHbase.scala
+++ src/main/scala/com/dahua/tag/TagRptToHbase.scala
@@ -1,13 +1,21 @@
 package com.dahua.tag
 
 import com.dahua.utils.TagUtil
-import org.apache.spark.SparkConf
+import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.hbase.client.{Admin, Connection, ConnectionFactory, Put}
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.hadoop.hbase.mapred.TableOutputFormat
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.{HColumnDescriptor, HTableDescriptor, TableName}
+import org.apache.hadoop.mapred.JobConf
 import org.apache.spark.broadcast.Broadcast
-import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
-
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.{SparkConf, SparkContext}
 import java.util.UUID
 
-object TagRpt {
+object TagRptToHbase {
 
   def main(args: Array[String]): Unit = {
     if (args.length != 4) {
@@ -30,6 +38,27 @@ object TagRpt {
     var sc = spark.sparkContext
 
     import spark.implicits._
+
+    //连接Hbase，并创建表。
+    val load: Config = ConfigFactory.load()
+    val hbaseTableName: String = load.getString("hbase.table.name")
+    val configuration: Configuration = sc.hadoopConfiguration
+    configuration.set("hbase.zookeeper.quorum",load.getString("hbase.zookeeper.host"))
+    val hbConn: Connection = ConnectionFactory.createConnection(configuration)
+    val hbAdmin: Admin = hbConn.getAdmin
+    if(!hbAdmin.tableExists(TableName.valueOf(hbaseTableName))){
+      println("表不存在，创建中****")
+      val tableDescriptor = new HTableDescriptor(TableName.valueOf(hbaseTableName))
+      val columnFamily = new HColumnDescriptor("tags")
+      tableDescriptor.addFamily(columnFamily)
+      hbAdmin.createTable(tableDescriptor)
+      hbAdmin.close()
+      hbConn.close()
+    }
+
+    val jobConf = new JobConf(configuration)
+    jobConf.setOutputFormat(classOf[TableOutputFormat])
+    jobConf.set(TableOutputFormat.OUTPUT_TABLE,hbaseTableName)
 
     //接收参数
     var Array(inputPath, app_Mapping, stopWords, outputPath) = args
@@ -75,7 +104,16 @@ object TagRpt {
     })
     TagDS.rdd.reduceByKey((list1, list2) => {
       (list1 ++ list2).groupBy(_._1).mapValues(_.foldLeft(0)(_ + _._2)).toList
-    }).saveAsTextFile(outputPath)
+    }).map{
+      case(userId,userTags)=>{
+        val put = new Put(Bytes.toBytes(userId))
+        val tags:String=userTags.map(t=>t._1+","+t._2).mkString(",")
+        var day="2020-09-30"
+
+        put.addImmutable(Bytes.toBytes("tags"),Bytes.toBytes(s"day${day}"),Bytes.toBytes(tags))
+        (new ImmutableBytesWritable(),put)
+      }
+    }.saveAsHadoopDataset(jobConf)
 
   }
 
